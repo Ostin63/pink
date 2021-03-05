3890b723d896c5f94a2b1177bb6f628c042a8fff
Ostin63 <39737185+Ostin63@users.noreply.github.com>
2021-03-05 16:42:10 +0300

    исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -30,20 +30,19 @@ const styles = () => {
     .pipe(sync.stream());
 }
 
-
 exports.styles = styles;
 
 //HTML
 
 const html = () => {
   return gulp.src("source/*.html")
-  .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
   .pipe(gulp.dest("build"))
 }
 
 exports.html = html;
-
-
 
 // Images
 
